c95dd61568fd177894e3ee94a82173a9845fadc1
henrique1232H <henriqueau111@gmail.com>
2023-12-12 13:27:41 -0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+const removeContainer = () => {
+    const main = document.querySelector("main div");
+    const title = document.querySelector(".title");
+
+    const widthScreen = window.innerWidth;
+
+    if(widthScreen  <= 700) {
+        main.classList.remove("container");
+        title.classList.add("container");
+    } else {
+        main.classList.add("container")
+        title.classList.remove("container")
+    }
+
+}
+setInterval(() => {
+    removeContainer();
+}, 200)
